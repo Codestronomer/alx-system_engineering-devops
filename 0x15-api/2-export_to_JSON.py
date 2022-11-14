074c2fdb8d38bbd6d4689f6fbbd5624ac845897b
codestronomer <johnrumide6@gmail.com>
2022-11-14 23:33:03 +0100

    fix file

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -4,30 +4,26 @@ and saves to a CSV file
 """
 import json
 import requests
+import sys
 
 
 if __name__ == "__main__":
+    userid = sys.argv[1]
     # checks if employee exists and retrieves employee name
-    url1 = 'https://jsonplaceholder.typicode.com/users/'
+    url1 = 'https://jsonplaceholder.typicode.com/users/'.format(userid)
+    r1 = requests.get(url1).json()
+    name = r1.get("username")
 
-    users = requests.get(url1).json()
-    users_dict = {}
-    for user in users:
-        id = user.get("id")
-        name = user.get("username")
-
-        # checks for current employee todos
-        url2 = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(id)
-        r2 = requests.get(url2).json()
-        list_row = []
-        for i in r2:
-            list_row.append({
-                "username": name,
-                "task": i.get("title"),
-                "completed": i.get("completed"),
-            })
-        users_dict[id] = list_row
+    # checks for current employee todos
+    url2 = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(userid)
+    r2 = requests.get(url2).json()
+    list_row = []
+    for i in r2:
+        list_row.append({"task": i.get("title"),
+                         "completed": i.get("completed"),
+                         "username": name})
+    employee_dict = {userid: list_row}
 
     filename = "{}.json".format(userid)
     with open(filename, 'w') as file:
-        json.dump(users_dict, file)
+        json.dump(employee_dict, file)
