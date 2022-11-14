b38ce466ea5d3c9372a6c4dde46253d199e0507d
codestronomer <johnrumide6@gmail.com>
2022-11-14 22:11:25 +0100

    [fix] clean up code

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -6,21 +6,19 @@ import sys
 
 
 if __name__ == "__main__":
+    userid = sys.argv[1]
     # checks if employee exists and retrieves employee name
-    url1 = 'https://jsonplaceholder.typicode.com/users/{}'.format(
-        sys.argv[1])
+    url1 = 'https://jsonplaceholder.typicode.com/users/{}'.format(userid)
     r1 = requests.get(url1).json()
     name = r1.get("username")
 
     # checks for current employee todos
-    url2 = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(
-        sys.argv[1])
+    url2 = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(userid)
     r2 = requests.get(url2).json()
     list_row = []
     for i in r2:
-        list_row.append(
-            [sys.argv[1], name, i.get("completed"), i.get("title")])
-    filename = "{}.csv".format(sys.argv[1])
+        list_row.append([userid, name, i.get("completed"), i.get("title")])
+    filename = "{}.csv".format(userid)
     with open(filename, 'w') as file:
         writer = csv.writer(file, delimiter=",",
                             quotechar='"', quoting=csv.QUOTE_ALL)
