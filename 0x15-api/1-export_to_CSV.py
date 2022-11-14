3c047f9203aa518352dc33713f1d6792e8c9fd4a
codestronomer <johnrumide6@gmail.com>
2022-11-14 21:57:35 +0100

    [update] add 1-export_to_CSV.py

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Script that uses JSONPlaceholder API to get information about employee """
+import csv
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    # checks if employee exists and retrieves employee name
+    url1 = 'https://jsonplaceholder.typicode.com/users/{}'.format(
+        sys.argv[1])
+    r1 = requests.get(url1).json()
+    name = r1.get("username")
+
+    # checks for current employee todos
+    url2 = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(
+        sys.argv[1])
+    r2 = requests.get(url2).json()
+    filename = "{}.csv".format(sys.argv[1])
+    with open(filename, 'w') as file:
+        writer = csv.writer(file)
+        for i in r2:
+            row = [sys.argv[1], name]
+            row.append(i.get("completed"))
+            row.append(i.get("title"))
+            writer.writerow(row)
