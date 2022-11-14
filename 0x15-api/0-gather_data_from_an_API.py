74d468366c6c59f9c083fe9606a10765c9b8614f
codestronomer <johnrumide6@gmail.com>
2022-11-14 12:13:46 -0800

    [fix] changes dictionary retrieval notation

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -12,15 +12,15 @@ if __name__ == '__main__':
         url1 = 'https://jsonplaceholder.typicode.com/users/{}'.format(
             sys.argv[1])
         r1 = requests.get(url1).json()
-        name = r1["name"]
+        name = r1.get("name")
 
         # checks for current employee todos
         url2 = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(
             sys.argv[1])
         r2 = requests.get(url2).json()
-        complete = [i for i in r2 if i["completed"] == True]
+        complete = [i for i in r2 if i.get("completed") == True]
         print(
             "Employee {} is done with tasks({}/{}):".format(
                 name, len(complete), (len(r2)), end="")
         for i in complete:
-            print("\t {}".format(i["title"]))
+            print("\t {}".format(i.get("title")))
